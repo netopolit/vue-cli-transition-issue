22a5f25018382615b64af5604e05d46721c50c2e
Mislav Paparella <mislav.paparella@gmail.com>
2021-03-02 20:32:23 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="demo">
+    <button @click="show = !show">Toggle show</button>
+    <transition name="bounce">
+      <p v-if="show">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+        facilisis enim libero, at lacinia diam fermentum id. Pellentesque
+        habitant morbi tristique senectus et netus.
+      </p>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  data() {
+    return {
+      show: true
+    };
+  }
+});
+</script>
